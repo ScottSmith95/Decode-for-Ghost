868cae763ea5ead81981704af8aa429426460b83
Scott Smith <mail@ScottHSmith.com>
2013-09-21 12:04:44 -0500

    Removed uncessary script

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,10 +8,6 @@
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
 
     });
 
