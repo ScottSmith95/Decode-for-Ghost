160b1cc0cbd47966f1dad8c094f50700dd8509c6
Denis Bukashk0zzz <Bukashk0zzz@me.com>
2014-03-24 11:27:58 +0200

    Add go tags. Remove sharing and comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,3 @@
 $( document ).ready(function() {
-
-    // For some reason moot not work correct when facebook enabled
-    new Share(".shareButtons", {
-        networks: {
-            facebook: {
-                enabled: false,
-                load_sdk: false
-            }
-        }
-    });
-
     $(".site-content, .site-main").fitVids();
 });
