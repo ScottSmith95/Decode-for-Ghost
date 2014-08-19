5e6a25017f03b5dc21c27802454f9b440c968211
Scott Smith <mail@ScottHSmith.com>
2014-08-18 23:57:39 -0500

    Added Gulp build system and changed folder structure

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp       = require('gulp'),
+	csscomb    = require('gulp-csscomb'),
+	sass       = require('gulp-sass'),
+	prefix     = require('gulp-autoprefixer'),
+	minify     = require('gulp-minify-css'),
+	bower      = require('gulp-bower');
+	concat     = require('gulp-concat'),
+	uglify     = require('gulp-uglify'),
+	sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+	styles:     'assets/styles/*.scss',
+	scripts:    ['assets/scripts/main.js', 'bower_components/FitVids/jquery.fitvids.js'],
+};
+
+gulp.task('bower', function() {
+	return bower()
+});
+
+gulp.task('styles', function() {
+	return gulp.src(paths.styles)
+		.pipe(sourcemaps.init())
+			.pipe(csscomb())
+			.pipe(sass())
+			.pipe(prefix('last 2 versions', '> 1%', 'ie 9', 'ie 8', 'Firefox ESR', 'Opera 12.1'))
+			.pipe(minify({cache: true}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('assets/styles/build/'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src(paths.scripts)
+		.pipe(sourcemaps.init())
+			.pipe(concat('main.js'))
+			.pipe(uglify())
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('assets/scripts/build/'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.styles, ['styles']);
+	gulp.watch(paths.scripts, ['scripts']);
+});
+
+// Workflows
+// $ gulp: Builds, prefixes, and minifies CSS files; concencates and minifies JS files; watches for changes. The works.
+gulp.task('default', ['bower', 'styles', 'scripts', 'watch']);
+
+// $ gulp build: Builds, prefixes, and minifies CSS files; concencates and minifies JS files. For deployments.
+gulp.task('default', ['bower', 'styles', 'scripts']);
